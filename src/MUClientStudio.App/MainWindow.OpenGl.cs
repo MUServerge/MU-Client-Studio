6627a1908a6b972dd745a728983de2f2b85ba192
MUIberia.com <muserver.ge@gmail.com>
2026-09-06 01:22:02 +0200

    Fix OpenGL viewport Player model reference

--- src/MUClientStudio.App/MainWindow.OpenGl.cs
+++ src/MUClientStudio.App/MainWindow.OpenGl.cs
@@ -2,6 +2,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media;
+using MUClientStudio.Models.Player;
 using MUClientStudio.Rendering.Player;
 using OpenTK.Wpf;
 
